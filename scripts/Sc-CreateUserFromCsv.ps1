545c884e6f73deb0076d7e77a80b0a6882fbe57a
lycamhao <sugiahoabinh2010@gmail.com>
2025-08-14 00:17:01 +0700

    commit

---
+++ scripts/Sc-CreateUserFromCsv.ps1
@@ -0,0 +1,15 @@
+Import-Module "$PSScriptRoot\..\modules\UserManagement.psm1" -Force
+$configs = Get-Content -Path "$PSScriptRoot\..\configs\configs.json" | ConvertFrom-Json
+$secrets = Get-Content -Path "$PSScriptRoot\..\configs\secrets.json" | ConvertFrom-Json
+if ($secrets.adServer.location -eq "remote")
+{
+    $pssession = Get-PSSession | Where-Object { $_.ComputerName -eq $secrets.adServer.host } | Select-Object 
+    # if (-not $pssession) {
+    #     cmConnectToRemoteAD -adServer $secrets.adServer.host -adUser $secrets.adServer.user
+    # }
+    # else {
+    #     Write-Host "Already connected to remote AD server: $($secrets.adServer.host)"
+    # }
+}
+
+# newADUserFromCsv -csvFile "$($configs.paths.data)\new-employees.csv"
